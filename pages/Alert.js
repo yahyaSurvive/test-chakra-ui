be234f9be62006654b59b525887b00d743d30118
yahyaSurvive <putuyahya5566@gmail.com>
2023-01-19 10:53:15 +0800

    a

--- pages/Alert.js
+++ pages/Alert.js
@@ -15,7 +15,7 @@ const TestAlert = () => {
     }
   };
 
-  const So123 = () => {
+  const So = () => {
     return (
       <Alert status="success" visibility={al === true ? "visible" : "hidden"}>
         <AlertIcon />
